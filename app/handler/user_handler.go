9ae5cb85ea11e9017a1ca8a2292bb458f9d9161c
opchaves <opaulochaves@gmail.com>
2023-10-18 10:36:46 -0300

    remove unused function

--- app/handler/user_handler.go
+++ app/handler/user_handler.go
@@ -1,9 +1,7 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
-	"reflect"
 
 	"github.com/gin-gonic/gin"
 	"github.com/opchaves/gin-web-app/app/model/apperrors"
@@ -33,25 +31,4 @@ func (h *Handler) Register(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, gin.H{"data": user})
 	return
-}
-
-func msgForTag(tag string, param string, t reflect.Type) string {
-	switch tag {
-	case "required":
-		return "This field is required"
-	case "email":
-		return "Invalid email"
-	case "min":
-		if t.Name() == "string" {
-			return fmt.Sprintf("cannot have less than %s characters", param)
-		}
-		return fmt.Sprintf("min value is %s, %s", param, t)
-	case "max":
-		if t.Name() == "string" {
-			return fmt.Sprintf("cannot be longer than %s characters", param)
-		}
-		return fmt.Sprintf("max value is %s", param)
-	}
-
-	return ""
 }
